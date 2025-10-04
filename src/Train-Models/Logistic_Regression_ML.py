1b6bf8d561878b0dce385ae6252c917c84abbab2
ghost <33994205+stadiumphenom@users.noreply.github.com>
2025-10-03 18:02:37 -0700

    Update Logistic_Regression_ML.py

--- src/Train-Models/Logistic_Regression_ML.py
+++ src/Train-Models/Logistic_Regression_ML.py
@@ -1,44 +1,24 @@
-import sqlite3
+import sqlite3, joblib
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
-import joblib
+from src.Utils.config_loader import load_config
 
-DB_PATH = "../../Data/dataset.sqlite"
-TABLE_NAME = "features_all"
+config = load_config()
+db_path, model_path = config["data"]["db_path"], config["models"]["log_ml"]
 
-def load_data():
-    conn = sqlite3.connect(DB_PATH)
-    df = pd.read_sql_query(f"SELECT * FROM {TABLE_NAME}", conn)
-    conn.close()
+con = sqlite3.connect(db_path)
+data = pd.read_sql_query("SELECT * FROM features_all", con)
+con.close()
 
-    # Target = Home Win (1 if home team wins, 0 if away wins)
-    y = df["home_win"].astype(int)
+y = data["home_win"]
+X = data.drop(columns=["home_win", "ou_cover", "gameday", "home_team", "away_team"]).values.astype(float)
 
-    # Features = drop identifiers + labels
-    X = df.drop(columns=["home_win", "ou_cover", "gameday", "home_team", "away_team"])
-    return X.values.astype(float), y.values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+model = LogisticRegression(max_iter=1000).fit(X_train, y_train)
 
+print(f"Accuracy: {accuracy_score(y_test, model.predict(X_test))}")
+print(classification_report(y_test, model.predict(X_test)))
 
-if __name__ == "__main__":
-    X, y = load_data()
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-
-    model = LogisticRegression(max_iter=500)
-
-    # Train model
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-
-    accuracy = accuracy_score(y_test, y_pred)
-    report = classification_report(y_test, y_pred)
-
-    print(f"Accuracy: {accuracy:.3f}")
-    print("Classification Report:")
-    print(report)
-
-    # Save trained model
-    joblib.dump(model, "../../Models/Logistic_Models/LogReg_NFL_ML.pkl")
-    print("Model saved to ../../Models/Logistic_Models/LogReg_NFL_ML.pkl")
+joblib.dump(model, model_path)
